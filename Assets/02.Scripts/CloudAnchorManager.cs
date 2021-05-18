ef53ca9d4aa912b31f2de68e758f58873f378a90
Lee Jae Hyun <myleje@gmail.com>
2021-05-18 10:34:18 +0900

    .

--- Assets/02.Scripts/CloudAnchorManager.cs
+++ Assets/02.Scripts/CloudAnchorManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsytems;
 
 public class CloudAnchorManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
